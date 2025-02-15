f1cd35c9018632c84cc9fc1df4b436f744b29fde
Penguin <89834270+trashpenguin@users.noreply.github.com>
2025-02-15 10:47:19 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const title = document.querySelector('.title');
+const text = 'I Have Something'.split('');
+
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+    title.innerHTML += `<span>${text[index]}</span>`;
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='margin-right: 20px;'></span>`;
   }
 }
 
 const textElements = document.querySelectorAll('.title span');
 textElements.forEach((element) => {
-  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  const randomDelay = Math.random() * 3; // Random delay between 0 and 3 seconds
   element.style.animationDelay = `${randomDelay}s`;
 });
